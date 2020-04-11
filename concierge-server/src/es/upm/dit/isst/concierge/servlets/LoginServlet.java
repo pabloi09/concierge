79458a01b5b5a749117872b2c4b471c182a6d65e
Patricia Centenera Béjar <patriciacentenerabejar@gmail.com>
2020-04-12 01:21:31 +0200

    Fixed errors and warnings

--- concierge-server/src/es/upm/dit/isst/concierge/servlets/LoginServlet.java
+++ concierge-server/src/es/upm/dit/isst/concierge/servlets/LoginServlet.java
@@ -20,6 +20,9 @@ import java.io.StringReader;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         StringBuilder buffer = new StringBuilder();
