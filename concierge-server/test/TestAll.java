744c84f189150b1e7db1fa51bcbf21977f0095ae
cgm <c.garcia-maurino@alumnos.upm.es>
2020-04-28 17:57:51 +0200

    [chore] Basic testing structure.

---
+++ concierge-server/test/TestAll.java
@@ -0,0 +1,12 @@
+package es.upm.dit.isst.concierge;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class JUnit5ExampleTest {
+
+    @Test
+    void justAnExample() {
+
+    }
+}
