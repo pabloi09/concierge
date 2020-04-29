5975b91dbdbfb5ad97236dda3513fe9f6b76123e
cgm <c.garcia-maurino@alumnos.upm.es>
2020-04-29 18:13:32 +0200

    [chore] Finished backend tests.

--- concierge-server/test/TestAll.java
+++ concierge-server/test/TestAll.java
@@ -1,15 +1,21 @@
 package es.upm.dit.isst.concierge;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 
 import es.upm.dit.isst.concierge.model.Cliente;
+import es.upm.dit.isst.concierge.model.Empleado;
 import es.upm.dit.isst.concierge.model.Habitacion;
+import es.upm.dit.isst.concierge.model.Mensaje;
 import es.upm.dit.isst.concierge.model.Solicitud;
 
 import es.upm.dit.isst.concierge.dao.ClienteDAO;
 import es.upm.dit.isst.concierge.dao.ClienteDAOImplementation;
+import es.upm.dit.isst.concierge.dao.EmpleadoDAO;
+import es.upm.dit.isst.concierge.dao.EmpleadoDAOImplementation;
 import es.upm.dit.isst.concierge.dao.SolicitudDAO;
 import es.upm.dit.isst.concierge.dao.SolicitudDAOImplementation;
 
@@ -28,6 +34,7 @@ class JUnit5ExampleTest {
         Habitacion habitacion = new Habitacion();
         habitacion.setNum(1);
         cliente.setHabitacion(habitacion);
+        clientedao.create(cliente);
 
         Cliente cliente_login = clientedao.login("00000001T");
         Cliente cliente_fail = clientedao.login("");
@@ -44,10 +51,21 @@ class JUnit5ExampleTest {
 
         solicitud.setTitulo("Solicitud de prueba");
         solicitud.setEstado("enviada");
+
         ClienteDAO clientedao = ClienteDAOImplementation.getInstance();
-        Cliente cliente_login = clientedao.login("05452329H");
+        Cliente cliente_login = clientedao.login("00000001A");
         solicitud.setCliente(cliente_login);
 
+        EmpleadoDAO empleadodao = EmpleadoDAOImplementation.getInstance();
+        Empleado empleado_1 = empleadodao.read(1);
+        solicitud.setEmpleado(empleado_1);
+
+        Mensaje mensaje_1 = new Mensaje();
+        List<Mensaje> mensajes = Arrays.asList(mensaje_1);
+        solicitud.setMensajes(mensajes);
+
         assertEquals(solicitud.getCliente(), cliente_login);
+        assertEquals(solicitud.getEmpleado().getName(), empleado_1.getName());
+        assertEquals(solicitud.getMensajes(), mensajes);
     }
 }
