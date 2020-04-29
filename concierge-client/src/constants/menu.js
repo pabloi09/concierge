87008122fd4631f44a50fa22fd4ec089d0f630ad
diegovf98 <diego.devegaf@alumnos.upm.es>
2020-04-29 19:03:19 +0200

    Room service menu completed

--- concierge-client/src/constants/menu.js
+++ concierge-client/src/constants/menu.js
@@ -1,12 +1,12 @@
 import carta from "./carta.json"
-const header = "Como cliente solicito un/una "
+const header = "Como cliente solicito: \n"
 var array = []
 Object.keys(carta).forEach((key)=>{
     array.push({title:key,meals:carta[key]})
 })
 export const menu = array.map(section=>{
     section.meals = section.meals.map(meal=>{
-        meal.value = header + meal.descripcion
+        meal.value = meal.descripcion
         return meal
     })
     return section
@@ -14,5 +14,13 @@ export const menu = array.map(section=>{
 
 
 export const getJson = (values)=>{
-    return {mensaje: values.element + ".\n" + values.comment, titulo: "Productos de la carta"}
+    return {mensaje: getItems(values.menu) + "\n" + values.comment, titulo: "Productos de la carta"}
+}
+
+function getItems(items){
+    var result = header + ""
+    items.forEach(item=>{
+        result += "- " + item.number + " de " + item.item.descripcion + "\n" 
+    })
+    return result
 }
